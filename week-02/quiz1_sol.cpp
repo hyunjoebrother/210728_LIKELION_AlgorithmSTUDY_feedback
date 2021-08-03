e27db4aa476bc436b4ab7fb714924d0e770875b9
hyunjoebrother <hyunjoe31@gmail.com>
2021-08-03 23:48:59 +0900

    210803_implement week02-quiz1

--- week-02/quiz1_sol.cpp
+++ week-02/quiz1_sol.cpp
@@ -26,4 +26,46 @@ int solution(vector<int> nums)
 
 
 // feedback - 2 (LJH)
-// 
+// 객체 종류 상관없이 최대한 많이 고르는 거라서 중복은 신경 안 써도 됨
+// set을 만들고 insert로 중복되는 것들을 하나의 객체로 통합
+// 고르는 갯수보다 set 크기가 크면 고르는 갯수가 최댓값
+// set 크기가 더 작으면 set의 크기를 최댓값으로 return
+
+//// set STL
+// * 시퀀스 container : vector, list, deque - 원소 자체를 보관
+// * 연관 container : set, multiset, map, multimap - key-value 구조를 가짐
+
+// - Node 기반 container, 균형 이진트리로 구현, 멤버 변수와 생성자 등이 거의 같음
+
+// : set의 원소 : key -> 삽입되는 key들은 자동으로 오름차순 정렬된다
+// : 원소들은 모두 유일하도록 저장됨 중복 x (중복저장은 multiset)
+// : set은 key의 존재 유무만 궁금하면 쓰기! map은 memory 큼 ㅇㅇ
+
+
+#include <vector>
+#include <set> // 중복 포켓몬들은 신경 안 써도 될 거 같아서 ㅇㅇ
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    set<int> poketmon;
+
+    int pick = nums.size()/2;
+    int size = nums.size();
+
+    // set에 insert하면서 중복 개체 제거
+    for (int i=0; i<size; i++)
+    {
+        poketmon.insert(nums[i]);
+    }
+
+    // set의 크기 < 고르는 갯수 -> set의 크기 return
+    if (poketmon.size() <= pick)
+        answer = poketmon.size();
+    else
+        answer = pick;
+        
+
+    return answer;
+}
